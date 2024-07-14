2d407a425088d3a680af85811e631893dd00d942
Valentina <valentinapescarav@gmail.com>
2024-07-14 13:18:16 -0300

    vercel jsonserver

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-export const api = axios.create ({
-    baseURL: 'http://localhost:3000'
-})
+const baseURL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api';
+
+export const api = axios.create({
+    baseURL: baseURL
+});
 
 export const buscar = async (url, setData) => {
-    const respuesta = await api.get(url)
-    setData(respuesta.data)
-}
+    const respuesta = await api.get(url);
+    setData(respuesta.data);
+};
